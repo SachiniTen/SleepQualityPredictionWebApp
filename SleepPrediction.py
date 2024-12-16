046d41bd3af478dd8e919d59799542577345481b
SachiniTen <53080091+SachiniTen@users.noreply.github.com>
2024-12-16 23:55:05 +0530

    Update SleepPrediction.py

--- SleepPrediction.py
+++ SleepPrediction.py
@@ -139,7 +139,9 @@ class RecommendationEngine:
                     print("\n")
                     
                     recommendations.append(
-                        f"•󠀠󠀠ㅤㅤㅤㅤㅤㅤㅤ Switching to {content} may change the sleep score to {new_score}.\n")
+                        f"󠀠󠀠󠀩󠁕
+      ㅤㅤㅤㅤㅤㅤㅤ
+     •󠀠󠀠Switching to {content} may change the sleep score to {new_score}.\n")
 
             if recommendations:
                 return "\n".join(recommendations)
